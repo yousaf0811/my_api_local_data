917399593cf2de783953dcd93772b47f35abd833
yousaf0811 <yousaf0811@gmail.com>
2022-11-28 13:18:47 +0500

    how to create local API

---
+++ read_me.js
@@ -0,0 +1,5 @@
+// npm init --yes
+// npm i json-server
+// write the comand in the package.json file "start": "npx json-server -p 3002 -w my_api.json",
+// npm start
+// write the address in the crome then data show.
